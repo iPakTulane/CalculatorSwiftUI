561b651c4ca663d8c00744b7b128f38411c82556
iPakTulane <ipak.tulane@gmail.com>
2023-08-23 17:17:46 +0500

    Add buttons and modifiers

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -7,7 +7,37 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case equal = "="
+    case add = "+"
+    case subtract = "-"
+    case multiply = "X"
+    case divide = "÷"
+    case decimal = "."
+    case clear = "AC"
+    case negative = "±"
+    case percent = "%"
+}
+
 struct ContentView: View {
+    
+    let buttons: [[CalcButton]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
     
     var body: some View {
         ZStack {
@@ -22,12 +52,30 @@ struct ContentView: View {
                         .font(.system(size: 64))
                         .foregroundColor(.white)
                 }
-                .padding() //to make room btw text and trailing edge
+                .padding() //to make a space btw text and trailing edge
+                
+                //Buttons
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                            }, label: {
+                                Text(item.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(Color.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            })
+                        }
+                    }
+
+                    
+                }
             }
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
